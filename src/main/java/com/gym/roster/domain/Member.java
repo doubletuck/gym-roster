247e0172f5277f17730f8684d3847b148ad9944a
whyceewhite <ycwhite@gmail.com>
2025-01-24 22:18:37 -0500

    Adjust lombak annotations

--- src/main/java/com/gym/roster/domain/Member.java
+++ src/main/java/com/gym/roster/domain/Member.java
@@ -1,20 +1,25 @@
 package com.gym.roster.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "member")
 public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false)
     private String lastName;
 }
