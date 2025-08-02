d8e799f6b71f35f5e0bc90fd15e0a6c7cc3a8195
whyceewhite <ycwhite@gmail.com>
2025-08-02 19:44:25 -0400

    Created a base entity class for common attributes.
    Named the foreign keys.
    
    Brought in the State and Country enums.

--- src/main/java/com/gym/roster/domain/CollegeConferenceHistory.java
+++ src/main/java/com/gym/roster/domain/CollegeConferenceHistory.java
@@ -4,9 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.EnumType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -15,28 +13,23 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.Instant;
-import java.util.UUID;
+import lombok.ToString;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 @Entity
-@Table(name = "college_conference_history", uniqueConstraints = {
+@Table(name = "college_conf_history", uniqueConstraints = {
         @UniqueConstraint(
                 name = "uk_college_conf_history",
                 columnNames = {"college_id", "start_year"})
 })
-public class CollegeConferenceHistory {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+public class CollegeConferenceHistory extends BaseEntity {
 
     @ManyToOne
-    @JoinColumn(name = "college_id", nullable = false)
+    @JoinColumn(name = "college_id", nullable = false, foreignKey = @ForeignKey(name = "fk_college_conf_history_college"))
     private College college;
 
     @Column(name = "start_year", nullable = false)
@@ -52,10 +45,4 @@ public class CollegeConferenceHistory {
     @Column(name = "division", nullable = false, length = 20)
     @Enumerated(EnumType.STRING)
     private Division division;
-
-    @Column(name = "creation_timestamp", nullable = false, updatable = false)
-    private Instant creationTimestamp;
-
-    @Column(name = "last_update_timestamp", nullable = false)
-    private Instant lastUpdateTimestamp;
 }
